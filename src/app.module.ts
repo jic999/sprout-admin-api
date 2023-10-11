fec395d90667fd535c13b750be8bdd5fb20818c6
jic999 <jic999@qq.com>
2023-10-11 17:14:14 +0800

    chore

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,7 +4,6 @@ import { ConfigModule, ConfigService } from '@nestjs/config'
 import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm'
 import { APP_GUARD, RouterModule } from '@nestjs/core'
 import { ServeStaticModule } from '@nestjs/serve-static'
-import { isDemoMode } from '@common/utils'
 import { AuthModule } from './auth'
 import { configuration } from './config'
 import { UserModule } from './user'
@@ -14,33 +13,28 @@ import { AdminModule } from './admin/admin.module'
 import { FileModule } from './file/file.module'
 import { EnvGuard } from './common/guards/env.guard'
 
-const imports = [
-  RouterModule.register(routes),
-  ConfigModule.forRoot({
-    isGlobal: true,
-    load: [configuration],
-  }),
-  TypeOrmModule.forRootAsync({
-    useFactory: (config: ConfigService) => ({
-      ...config.get<TypeOrmModuleOptions>('db'),
-    }),
-    inject: [ConfigService],
-  }),
-  UserModule,
-  AuthModule,
-  AdminModule,
-  FileModule,
-]
-
-!isDemoMode() && imports.push(
-  ServeStaticModule.forRoot({
-    rootPath: join(__dirname, '../static'),
-    serveRoot: '/static',
-  }),
-)
-
 @Module({
-  imports,
+  imports: [
+    RouterModule.register(routes),
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [configuration],
+    }),
+    TypeOrmModule.forRootAsync({
+      useFactory: (config: ConfigService) => ({
+        ...config.get<TypeOrmModuleOptions>('db'),
+      }),
+      inject: [ConfigService],
+    }),
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '../static'),
+      serveRoot: '/static',
+    }),
+    UserModule,
+    AuthModule,
+    AdminModule,
+    FileModule,
+  ],
   providers: [
     {
       provide: APP_GUARD,
