289b9954fafc589652abede6492e89845ad45250
jic999 <jic999@qq.com>
2023-10-13 14:44:40 +0800

    chore: config

--- src/config/envs/demo.ts
+++ src/config/envs/demo.ts
@@ -2,7 +2,7 @@ export const config = {
   db: {
     type: process.env.DB_TYPE || 'mysql',
     synchronize: true,
-    logging: true,
+    logging: false,
     host: process.env.DB_HOST || '127.0.0.1',
     port: process.env.DB_PORT || 3306,
     username: process.env.DB_USER || 'username',
